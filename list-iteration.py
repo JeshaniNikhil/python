f35cd07d0b6e286394e7d22910c3e2e3b1d501e6
JesaniNikhil <webnikhil26@gmail.com>
2023-12-04 18:43:59 +0530

    python code

---
+++ list-iteration.py
@@ -0,0 +1,8 @@
+l=[10,20,30,40,50];
+t=len(l)
+for a in l:
+    print(a);
+for b in range(t):
+    print(l[b])
+for c in range(t-1,-1,-1):
+    print(l[c])
---
+++ listfunction.py
@@ -0,0 +1,11 @@
+l=[20,30,40,50]
+# del l[1]
+# print(l)
+# l.pop(1)
+# print(l)
+# l.remove(50)
+# print(l)
+# l.clear()
+# print(l)
+l.insert(0,10)
+print(l)
